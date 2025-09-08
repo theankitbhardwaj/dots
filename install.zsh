ef2fd20974db9cef2d42db549d8b326f70ecc5d7
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2025-09-08 23:52:17 +0530

    Fixed install script, updated border color

--- install.zsh
+++ install.zsh
@@ -1,14 +1,5 @@
 #!/bin/zsh
 
-# Create config directories if they don't exist
-mkdir -p ~/.config/ghostty
-mkdir -p ~/.config/hypr
-mkdir -p ~/.config/kitty
-mkdir -p ~/.config/swaync
-mkdir -p ~/.config/waybar
-mkdir -p ~/.config/wlogout
-mkdir -p ~/.config/wofi
-
 # Symlink config files and directories
 ln -sf $(pwd)/ghostty ~/.config/ghostty
 ln -sf $(pwd)/hypr ~/.config/hypr
@@ -17,6 +8,6 @@ ln -sf $(pwd)/starship/starship.toml ~/.config/starship.toml
 ln -sf $(pwd)/swaync ~/.config/swaync
 ln -sf $(pwd)/waybar ~/.config/waybar
 ln -sf $(pwd)/wlogout ~/.config/wlogout
-ln -sf $(pwd)/wofi/style.css ~/.config/wofi/style.css
+ln -sf $(pwd)/wofi ~/.config/wofi
 
 echo "Dotfiles installed successfully!"
